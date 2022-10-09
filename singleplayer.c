8d797803d1e900571f2abb692ac4c645ec385585
Iheb Hadj Ali <57681055+ihebhadjali@users.noreply.github.com>
2022-10-09 17:26:32 +0200

    Update singleplayer.c

--- singleplayer.c
+++ singleplayer.c
@@ -1,23 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
 //#include"memory.h"
+void delay(int);
 int fillchoice();
-void fillGrille(char**,int,int);
-void showGrille(char**,int,int);
+void fillGrid(char**,int,int);
+void initialGrid(char**,int,int);
+void showGrid(char**,int,int);
 void singleplayer( int GameDiff ){
-    int i,l=2,nbcolGrille=2,j,FillChoice;
-    char **Grille;
+    int i,l=3,nbcolGrid=2,j,FillChoice;
+    char **Grid, **PlayerGrid;
     system("cls");
     printf("== Single Player ==\n");
-    Grille=(char **) malloc(l*sizeof(char*));
+    Grid=(char **) malloc(l*sizeof(char*));
     for (i=0;i<l;i++)
-        Grille[i]=(char *) malloc(nbcolGrille*sizeof(char));
+        Grid[i]=(char *) malloc(nbcolGrid*sizeof(char));
     FillChoice=fillchoice();
     if(FillChoice==1){
-        fillGrille(Grille,l,nbcolGrille);
+        fillGrid(Grid,l,nbcolGrid);
     }
-    showGrille(Grille,l,nbcolGrille);
-    free(Grille);
+    showGrid(Grid,l,nbcolGrid);
+    delay(5);
+    system("cls");
+    PlayerGrid=(char **) malloc(l*sizeof(char*));
+    for (i=0;i<l;i++)
+        PlayerGrid[i]=(char *) malloc(nbcolGrid*sizeof(char));
+    initialGrid(PlayerGrid,l,nbcolGrid);
+    printf("==> Game Started <==\nYou have 5 seconds to see the grid:\n");
+    printf("Ready?!\n");
+    delay(1);
+    printf("Go\n");
+    showGrid(Grid,l,nbcolGrid);
+    showGrid(PlayerGrid,l,nbcolGrid);
+    delay(3);
+    system("cls");
+    free(Grid);
 }
 int fillchoice(){
     int c;
@@ -28,39 +45,41 @@ int fillchoice(){
     }while (c!=1 && c!=2);
     return c;
 }
-void fillGrille(char **Grille,int l,int nbcolGrille){
+void fillGrid(char **Grid,int l,int nbcolGrid){
     int i,j;
-    printf("Enter les elements de la matrice A\n");
+    printf("===> Fill in the grid <===\n");
     for(i=0;i<l;i++)
-        for(j=0;j<nbcolGrille;j++){
-            printf("Enter la valeur de A[%d][%d] ",i,j);
-            scanf(" %c",&Grille[i][j]);
+        for(j=0;j<nbcolGrid;j++){
+            printf("Grid[%d][%d] ",i,j);
+            scanf(" %c",&Grid[i][j]);
             printf("\n");
         }
 }
-void showGrille(char **Grille,int l,int nbcolGrille){
+void initialGrid(char **Grid,int l,int nbcolGrid){
+    int i,j,k=49;
+    printf("===> Fill in the grid <===\n");
+    for(i=0;i<l;i++)
+        for(j=0;j<nbcolGrid;j++){
+            Grid[i][j]=(char)k;
+            k++;
+        }
+}
+void showGrid(char **Grid,int l,int nbcolGrid){
     int i,j;
-    printf("\n===> Grille <===\n");
-    for(j=0;j<nbcolGrille;j++) printf("________________");
+    printf("\n===> Grid <===\n");
+    for(j=0;j<nbcolGrid;j++) printf("________________");
     printf("\n");
     for (i=0;i<l;i++){
-        for(j=0;j<nbcolGrille;j++)
-        printf("\t%c\t|", Grille[i][j]);
+        for(j=0;j<nbcolGrid;j++)
+        printf("\t%c\t|", Grid[i][j]);
         printf("\n");
-        for(j=0;j<nbcolGrille;j++) printf("________________");
+        for(j=0;j<nbcolGrid;j++) printf("________________");
         printf("\n");
     }
 }
-void hiddenGrille(char **Grille,int l,int nbcolGrille){
-    int i,j;
-    printf("\n===> Hidden Grille <===\n");
-    for(j=0;j<nbcolGrille;j++) printf("________________");
-    printf("\n");
-    for (i=0;i<l;i++){
-        for(j=0;j<nbcolGrille;j++)
-        printf("\t%c\t|", Grille[i][j]);
-        printf("\n");
-        for(j=0;j<nbcolGrille;j++) printf("________________");
-        printf("\n");
-    }
+void delay(int number_of_seconds){
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds)
+        ;
 }
